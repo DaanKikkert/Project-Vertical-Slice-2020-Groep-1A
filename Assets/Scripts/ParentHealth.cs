9b59147f0b4878dec26b5b2331d63d05612e2e23
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2020-11-19 10:24:08 +0100

    added health system

---
+++ Assets/Scripts/ParentHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ParentHealth : MonoBehaviour
+{
+
+    [SerializeField] private float _startHealth = 5f;
+
+    private float _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _startHealth;
+    }
+
+    // Called from UnityEvent
+    //public void TakeDamage(float dmg = 1f)
+    //{
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+            
+        //}
+
+//    }
+
+    void Update()
+    {
+        float dmg = 1f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _currentHealth -= dmg;
+            print("Taking damage");
+            if (_currentHealth <= 0)
+            {
+                Death();
+            }
+
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+        print("Death");
+    }
+}
