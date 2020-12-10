7f93026264a36a8e8afb6f40ced2d2c0dc47894d
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2020-12-10 11:14:48 +0100

    Added PlayerHealth Script and update ParentHealth

--- Assets/Scripts/ParentHealth.cs
+++ Assets/Scripts/ParentHealth.cs
@@ -7,47 +7,34 @@ using UnityEngine.Events;
 public class ParentHealth : MonoBehaviour
 {
 
-    [SerializeField] private float _startHealth = 5f;
-    private float _currentHealth;
+    [SerializeField] private float _startHealth;
+    [SerializeField] private float _currentHealth;
+    [SerializeField] private float _dmg;
 
-    private void Start()
+
+    void Awake()
     {
+        //Zet de health die gebruikt word gelijk aan een standaard value
         _currentHealth = _startHealth;
     }
 
-    void Update()
-    { 
-        float dmg = 1f;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _currentHealth -= dmg;
-            print("Taking damage");
-            if (_currentHealth <= 0)
-            {
-                Death();
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage();
-        }
-    }
     public void TakeDamage()
     {
-        float dmg = 1f;
-        _currentHealth -= dmg;
-        print("Taking damage");
+        //Doet de damage door dmg van currentHealth af te halen
+        _currentHealth -= _dmg;
+        //Print zet de hp die over is in het console.
+        print(_currentHealth);
 
-        if (_currentHealth <= 0)
+        if (_currentHealth <= 0f)
         {
             Death();
-            print("Death");
         }
+
     }
 
-    public void Death()
+    void Death()
     {
-        Destroy(gameObject);
-        print("Death");
+        Destroy(this.gameObject);
+        print("death");
     }
 }
