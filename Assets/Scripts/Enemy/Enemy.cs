aa3d9a8230ef4be2f22acff762afb00aeee519ac
thomasberrens <48439074+thomasberrens@users.noreply.github.com>
2020-12-10 11:57:46 +0100

    Fixed enemy shooting, enemy movement

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = System.Random;
 
 public class Enemy : MonoBehaviour
 {
-  [SerializeField] private GameObject prefabBullet;
-  [SerializeField] private GameObject player;
-  [SerializeField] private GameObject shootPos;
-  [SerializeField] private float timeBetweenShots = 0.3333f; // 3 shots per second
-  [SerializeField] private float timestamp;
+    [SerializeField] private GameObject prefabBullet;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject shootPos;
+    [SerializeField] private float timeBetweenShots = 0.3333f; // 3 shots per second
+    [SerializeField] private float timestamp;
+    [SerializeField] private bool active;
+    [SerializeField] private int bulletSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +23,40 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Shoot();
+        // Debug purposes Shoot2(active);
     }
 
-    void Shoot()
+    void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.gameObject.tag == "bullet")
+        {
+            Random rnd = new Random();
+           
+            int chance = rnd.Next(1, 11);
+            Debug.Log(chance);
+           
+            if (!chance.Equals(10)) return;
+
+            GameObject bullet =
+                Instantiate(prefabBullet, shootPos.transform.position, Quaternion.identity) as GameObject;
+        //    timestamp = Time.time + timeBetweenShots;
+            
+            bullet.GetComponent<Rigidbody2D>().velocity = (player.transform.position - transform.position).normalized * bulletSpeed;
+        }
+    }
+    
+    void Shoot2(bool active)
+    {
+        if (!active) return;
+
         if (Time.time >= timestamp)
         {
-            Vector2 position = new Vector2(player.transform.position.x, player.transform.position.y);
-            
+
             GameObject bullet =
                 Instantiate(prefabBullet, shootPos.transform.position, Quaternion.identity) as GameObject;
             timestamp = Time.time + timeBetweenShots;
-            
-            bullet.transform.LookAt(position);
-            bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.forward * 1000);
+
+            bullet.GetComponent<Rigidbody2D>().velocity = (player.transform.position - transform.position).normalized * bulletSpeed;
         }
     }
 }
